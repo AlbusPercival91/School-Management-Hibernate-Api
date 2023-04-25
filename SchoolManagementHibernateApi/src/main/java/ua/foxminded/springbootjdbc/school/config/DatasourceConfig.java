e5ff3adfe81119332466b44b34b5e92a5f365c82
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-04-25 22:14:35 +0200

    1. create HibernateConfig class
    2. change DatasourseConfig class - remove JdbcTemplate from DataSource

--- SchoolManagementHibernateApi/src/main/java/ua/foxminded/springbootjdbc/school/config/DatasourceConfig.java
+++ SchoolManagementHibernateApi/src/main/java/ua/foxminded/springbootjdbc/school/config/DatasourceConfig.java
@@ -1,28 +1,19 @@
 package ua.foxminded.springbootjdbc.school.config;
 
-import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class DatasourceConfig {
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource.main")
-    public HikariDataSource hikariDataSource() {
-        return DataSourceBuilder
-                .create()
-                .type(HikariDataSource.class)
-                .build();
-    }
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(HikariDataSource hikariDataSource) {
-        return new JdbcTemplate(hikariDataSource);
-    }
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource.main")
+  public DataSource dataSource() {
+    return DataSourceBuilder.create().build();
+  }
 }
